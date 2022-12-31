0a705e026607a1b137b5df0e3c8c14e6a26f1fb7
mochan-tk <takashi1.kawamoto@gmail.com>
2022-12-31 13:23:40 +0000

    sample line bot

--- README.md
+++ README.md
@@ -12,3 +12,49 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 * `cdk deploy`      deploy this stack to your default AWS account/region
 * `cdk diff`        compare deployed stack with current state
 * `cdk synth`       emits the synthesized CloudFormation template
+
+
+## setup
+
+- Cloud 9 Instance Type t2.midium
+- Volume expanded (20G)
+
+```
+bash resize.sh 20
+```
+
+## Git
+
+```
+$ cd ~/.ssh
+
+$ ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+
+$ eval "$(ssh-agent -s)"
+
+$ ssh-add ~/.ssh/id_rsa
+
+### 公開鍵をGitHubに登録↓
+$ cat ~/.ssh/id_rsa.pub
+
+### 疎通確認
+$ ssh -T git@github.com
+
+$ git config --global user.name <user.name>
+$ git config --global user.email <user.email>
+
+$ git remote set-url origin <ssh url>
+```
+
+## Run
+
+```
+$ npm install
+$ cdk synth
+$ cdk bootstrap
+$ MSG_CHANNEL_SECRET=< your secret >
+$ MSG_CHANNEL_ACCESS_TOKEN=< your access token >
+$ cdk deploy \
+-c MSG_CHANNEL_SECRET=${MSG_CHANNEL_SECRET} \
+-c MSG_CHANNEL_ACCESS_TOKEN=${MSG_CHANNEL_ACCESS_TOKEN}
+```
--- lib/handson-line-bot-aws-stack.ts
+++ lib/handson-line-bot-aws-stack.ts
@@ -1,16 +1,36 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Runtime, FunctionUrlAuthType, HttpMethod } from "aws-cdk-lib/aws-lambda";
 
-export class HandsonLineBotAwsStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class HandsonLineBotAwsStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    
+    // https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_lambda_nodejs-readme.html
+    const lambdaFunction = new NodejsFunction(this, 'MyFunction', {
+      entry: 'app/index.js', // accepts .js, .jsx, .ts, .tsx and .mjs files
+      handler: 'handler', // defaults to 'handler'
+      depsLockFilePath: 'app/package-lock.json',
+      runtime: Runtime.NODEJS_16_X,
+      environment: {
+        MSG_CHANNEL_SECRET: this.node.tryGetContext('MSG_CHANNEL_SECRET'),
+        MSG_CHANNEL_ACCESS_TOKEN: this.node.tryGetContext('MSG_CHANNEL_ACCESS_TOKEN'),
+      },
+    });
+    
+    // https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_lambda.FunctionUrl.html
+    const lambdaURLs = lambdaFunction.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+      cors: {
+        allowedMethods: [HttpMethod.POST],
+        allowedOrigins: ['*'],
+      },
+    });
+    
+    new CfnOutput(this, 'FunctionUrl', {
+      value: lambdaURLs.url,
+    });
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'HandsonLineBotAwsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
